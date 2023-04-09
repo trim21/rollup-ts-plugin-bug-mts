1df91dd767daa8e264ab4d80d2a708cd27194cb9
Trim21 <trim21.me@gmail.com>
2023-04-10 04:16:27 +0800

    add mocha source-map-support

--- .mocharc.js
+++ .mocharc.js
@@ -4,6 +4,6 @@ module.exports = {
   reporter: 'spec',
   ui: 'bdd',
   timeout: 5000,
-  require: ['dotenv/config'],
+  require: ['dotenv/config', 'source-map-support/register'],
   'node-option': ['experimental-specifier-resolution=node', 'loader=ts-node/esm'],
 }
